c18d98af01477319184718da8c218c5349c76168
rodrigo <rodrigocf_92@hotmail.com>
2022-07-03 12:34:42 -0700

    Added more characters to hash

--- src/utils/generate_user_hash.py
+++ src/utils/generate_user_hash.py
@@ -6,5 +6,5 @@ def generate_user_hash(length=256):
     if not isinstance(length, int):
         raise ValueError('Length mut be of type int.')
 
-    unique_characters = string.ascii_uppercase + string.digits + '!@#$^&*()_+{}":?><~`|' + string.ascii_lowercase
+    unique_characters = string.ascii_uppercase + string.digits + '!@#$^*()_+{}:><~`|ñáéíóúü¡¿' + string.ascii_lowercase
     return ''.join(random.SystemRandom().choice(unique_characters) for _ in range(length))
