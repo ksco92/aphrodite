3065cf5d44b9bc3c83b6a2197851a9e0531d6dc6
rodri <rodrigocf_92@hotmail.com>
2022-07-02 23:57:43 -0700

    Reverted to pg8000, fixed connection bug

--- src/utils/get_conn.py
+++ src/utils/get_conn.py
@@ -8,10 +8,10 @@ from utils.get_secret import get_secret
 def get_conn(secret_name: str) -> Connection:
     if am_i_lambda():
         secret = get_secret(secret_name)
-        username = secret['username'],
-        password = secret['password'],
-        host = secret['host'],
-        dbname = secret['dbname'],
+        username = secret['username']
+        password = secret['password']
+        host = secret['host']
+        dbname = secret['dbname']
         port = int(secret['port'])
 
     else:
@@ -21,7 +21,7 @@ def get_conn(secret_name: str) -> Connection:
         dbname = 'aphrodite_local'
         port = 5432
 
-    db_url = f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}'
+    db_url = f'postgresql+pg8000://{username}:{password}@{host}:{port}/{dbname}'
     engine = create_engine(db_url)
     conn = engine.connect()
 
