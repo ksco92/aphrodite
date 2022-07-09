57e45a990239d3ff3889aa6e45c4ceff05dcbd03
rodrigo <rodrigocf_92@hotmail.com>
2022-07-09 13:45:39 -0700

    test

--- lib/ui/ui.ts
+++ lib/ui/ui.ts
@@ -1,12 +1,13 @@
-import {BlockPublicAccess, Bucket, BucketAccessControl} from 'aws-cdk-lib/aws-s3';
+import {BlockPublicAccess, Bucket} from 'aws-cdk-lib/aws-s3';
 import {Key} from 'aws-cdk-lib/aws-kms';
 import {Construct} from 'constructs';
-import {Duration, RemovalPolicy, Stack} from 'aws-cdk-lib';
-import {Distribution, OriginAccessIdentity} from 'aws-cdk-lib/aws-cloudfront';
+import {RemovalPolicy, Stack} from 'aws-cdk-lib';
+import {AllowedMethods, Distribution, OriginAccessIdentity, ViewerProtocolPolicy} from 'aws-cdk-lib/aws-cloudfront';
 import {S3Origin} from 'aws-cdk-lib/aws-cloudfront-origins';
 import {Certificate} from 'aws-cdk-lib/aws-certificatemanager';
-import {CnameRecord, HostedZone} from 'aws-cdk-lib/aws-route53';
+import {ARecord, HostedZone, RecordTarget} from 'aws-cdk-lib/aws-route53';
 import Constants from '../constants';
+import {CloudFrontTarget} from "aws-cdk-lib/aws-route53-targets";
 
 export default function makeUi(
     scope: Construct,
@@ -19,7 +20,7 @@ export default function makeUi(
 
     const uiBucket = new Bucket(scope, `${Constants.APP_NAME}${Constants.getStageName()}UIBucket`, {
         blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-        accessControl: BucketAccessControl.PRIVATE,
+        publicReadAccess: false,
         bucketName: `${Constants.APP_NAME}-${Constants.getStageName()}-ui-${Stack.of(scope).account}-${Stack.of(scope).account}`,
         encryptionKey: uiBucketKey,
         enforceSSL: true,
@@ -31,12 +32,22 @@ export default function makeUi(
 
     const uiDistribution = new Distribution(scope, `${Constants.APP_NAME}${Constants.getStageName()}Distribution`, {
         defaultBehavior: {
-            origin: new S3Origin(uiBucket),
+            origin: new S3Origin(uiBucket, {
+                originAccessIdentity,
+            }),
+            allowedMethods: AllowedMethods.ALLOW_ALL,
+            viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         },
         certificate,
         domainNames: [
             publicHostedZone.zoneName,
         ],
         defaultRootObject: 'index.html',
+    });
+
+    new ARecord(scope, `${Constants.APP_NAME}${Constants.getStageName()}DistributionARecord`, {
+        recordName: `${Constants.getStageName()}.${publicHostedZone.zoneName}`,
+        target: RecordTarget.fromAlias(new CloudFrontTarget(uiDistribution)),
+        zone: publicHostedZone,
     });
 }
